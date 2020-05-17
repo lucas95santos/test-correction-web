dc74b521ad8c908c35055b8a782662c02ebde1e1
lucas95santos <lucasa95santos@gmail.com>
2020-05-17 13:49:05 -0400

    Modificando o layout do dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,11 +4,14 @@ import { DashboardTop, AddNewClass } from '../../components';
 // styles
 import './Dashboard.css';
 // icons
-import { FiPlus } from 'react-icons/fi';
+import { FaPlus, FaUserGraduate } from 'react-icons/fa';
+// images
+import examSVG from '../../assets/images/svgs/exam.svg';
+import correctedExamSVG from '../../assets/images/svgs/corrected_exam.svg';
 // tooltip
 import ReactTooltip from 'react-tooltip';
 
-export default function Dashboard(props) {
+export default function Dashboard() {
   const [profileDropdown, setProfileDropDown] = useState(false);
   const [notificationDropdown, setNotificationDropDown] = useState(false);
   const [addNewClassOpen, setAddNewClassOpen] = useState(false);
@@ -23,6 +26,49 @@ export default function Dashboard(props) {
     }
   }
 
+  const students = [
+    {
+      name: 'João Carlos',
+      registration: '2016.1907.013-8',
+      class: 'C',
+      grade: '8',
+      educationLevel: 'Ensino Fundamental',
+      educationLeveInitials: 'EF'
+    },
+    {
+      name: 'Ana Carolina',
+      registration: '2016.1998.033-4',
+      class: 'B',
+      grade: '1',
+      educationLevel: 'Ensino Médio',
+      educationLeveInitials: 'EM'
+    },
+    {
+      name: 'Ana Carolina',
+      registration: '2016.1998.033-4',
+      class: 'B',
+      grade: '1',
+      educationLevel: 'Ensino Médio',
+      educationLeveInitials: 'EM'
+    },
+    {
+      name: 'Ana Carolina',
+      registration: '2016.1998.033-4',
+      class: 'B',
+      grade: '1',
+      educationLevel: 'Ensino Médio',
+      educationLeveInitials: 'EM'
+    },
+    {
+      name: 'Ana Carolina',
+      registration: '2016.1998.033-4',
+      class: 'B',
+      grade: '1',
+      educationLevel: 'Ensino Médio',
+      educationLeveInitials: 'EM'
+    }
+  ]
+
   return (
     <div className="dashboard" onClick={() => handleClickOut()}>
       <DashboardTop
@@ -33,19 +79,54 @@ export default function Dashboard(props) {
         notificationAmount={0}
       />
 
+      <div className="dashboard__title">
+        <h1>Painel de Controle</h1>
+      </div>
+
       <div className="dashboard__content">
-        <div className="card classes">
+        <div className="card exams">
           <div className="card__title">
-            <h1>Turmas</h1>
+            <h1>Provas</h1>
           </div>
           <div className="card__body">
             <div
-              className="card__item"
-              data-tip="Adicionar nova turma"
-              onClick={() => setAddNewClassOpen(true)}
+              className="card__item card__item--exam"
+            >
+              <div className="exam__content">
+                <div className="exam__image">
+                  <img src={correctedExamSVG} alt="Exam"/>
+                </div>
+                <div className="exam__info">
+                  <p className="exam__title">Prova 1</p>
+                  <p className="info__item"><span>Quantidade de questões: </span> 10</p>
+                  <p className="info__item"><span>Quantidade de alunos: </span> 30</p>
+                  <p className="info__item"><span>Data de aplicação: </span> 03/05/2020</p>
+                  <p className="info__item"><span>Situação: </span>Corrigida</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="card__item card__item--exam"
+            >
+              <div className="exam__content">
+                <div className="exam__image">
+                  <img src={examSVG} alt="Exam"/>
+                </div>
+                <div className="exam__info">
+                  <p className="exam__title">Prova 2</p>
+                  <p className="info__item"><span>Quantidade de questões: </span> 10</p>
+                  <p className="info__item"><span>Quantidade de alunos: </span> 30</p>
+                  <p className="info__item"><span>Data de aplicação: </span> 03/05/2020</p>
+                  <p className="info__item"><span>Situação: </span>Aguardando correção</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="card__item card__item--add"
+              data-tip="Adicionar nova prova"
             >
               <div className="add-sign">
-                <FiPlus size={20} color="#dc7037" />
+                <FaPlus size={18} color="#dc7037" />
               </div>
             </div>
           </div>
@@ -55,42 +136,54 @@ export default function Dashboard(props) {
             <h1>Alunos</h1>
           </div>
           <div className="card__body card__body--table">
-            <table>
-              <thead>
-                <tr>
-                  <th>Nome</th>
-                  <th>Matrícula</th>
-                  <th>Série</th>
-                  <th>Turma</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>John Doe</td>
-                  <td>2016.1907.013-8</td>
-                  <td>8ª</td>
-                  <td>C</td>
-                </tr>
+            <div className="table-responsive">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>Matrícula</th>
+                    <th className="text-center">Série</th>
+                    <th className="text-center">Turma</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {students.map(student => (
+                    <tr>
+                      <td>{student.name}</td>
+                      <td>{student.registration}</td>
+                      <td className="text-center">{`${student.grade}° - ${student.educationLeveInitials}`}</td>
+                      <td className="text-center">{student.class.toUpperCase()}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
 
-                <tr>
-                  <td>John Doe</td>
-                  <td>2016.1907.013-8</td>
-                  <td>8ª</td>
-                  <td>C</td>
-                </tr>
-              </tbody>
-            </table>
+            <button type="button" className="card__button btn-small">
+              <FaPlus size={12} />
+              Ver todos
+            </button>
+
+            <button type="button" className="card__button card__button--add btn-small">
+              <FaUserGraduate size={13} />
+              Adicionar aluno
+            </button>
           </div>
         </div>
-        <div className="card tests">
+        <div className="card classes">
           <div className="card__title">
-            <h1>Provas</h1>
+            <h1>Turmas</h1>
           </div>
-        </div>
-
-        <div className="card info">
-          <div className="card__title">
-            <h1>Outros</h1>
+          <div className="card__body">
+            <div
+              className="card__item card__item--add card__item--class"
+              data-tip="Adicionar nova turma"
+              onClick={() => setAddNewClassOpen(true)}
+            >
+              <div className="add-sign">
+                <FaPlus size={18} color="#dc7037" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -100,7 +193,7 @@ export default function Dashboard(props) {
         closeModal={() => setAddNewClassOpen(false)}
       />
 
-      <ReactTooltip place="right" type="dark" effect="solid" />
+      <ReactTooltip place="bottom" type="dark" effect="solid" />
     </div>
   );
 }
